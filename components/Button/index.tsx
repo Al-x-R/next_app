2fff8c45e0bf8934667ea2f6941006b54cba79e3
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-08-04 10:26:03 +0300

    Add webpack config for display svg as react component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import clsx from 'clsx';
 
 import { ButtonProps } from './Button.props';
+import ArrowIcon from './arrow.svg';
 
 import styles from './Button.module.css';
 
-const Button = ({ children, appearance, className, ...props }: ButtonProps): JSX.Element => {
+const Button = ({ children, appearance, className, arrow = 'none', ...props }: ButtonProps): JSX.Element => {
   return (
     <button
       className={clsx(styles.button, className, {
@@ -15,6 +16,14 @@ const Button = ({ children, appearance, className, ...props }: ButtonProps): JSX
       {...props}
     >
       {children}
+      {arrow !== 'none' && <span
+        className={clsx(styles.arrow, {
+        [styles.down]: arrow === 'down',
+        [styles.right]: arrow === 'right'
+      })}>
+        <ArrowIcon />
+      </span>
+      }
     </button>
   );
 };
