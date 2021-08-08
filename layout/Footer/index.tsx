5db5c5d891aa00f2f5a8c65642eb5a9749df5bcb
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-08-08 17:25:33 +0300

    Add basic layout footer

---
+++ layout/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import clsx from 'clsx';
+
+import { FooterProps } from './Footer.props';
+
+import styles from './Footer.module.css';
+
+const Footer = ({ ...props }: FooterProps): JSX.Element => {
+
+  return (
+    <>
+      Footer
+    </>
+  );
+};
+
+export default Footer;
