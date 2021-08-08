0b6e6263764b048eb07757f0f3044efb1fdd91e5
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-08-08 17:24:46 +0300

    Add layout header

---
+++ layout/Header/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import clsx from 'clsx';
+
+import { HeaderProps } from './Header.props';
+
+import styles from './Header.module.css';
+
+const Header = ({ ...props }: HeaderProps): JSX.Element => {
+
+  return (
+    <>
+      Header
+    </>
+  );
+};
+
+export default Header;
