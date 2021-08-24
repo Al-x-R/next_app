84864f59135ff9cfe41c40a2aadde2eb7634e11d
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-08-24 19:12:10 +0300

    Add footer layout

--- layout/Footer/index.tsx
+++ layout/Footer/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import clsx from 'clsx';
+import { format } from 'date-fns';
 
 import { FooterProps } from './Footer.props';
 
 import styles from './Footer.module.css';
 
-const Footer = ({ ...props }: FooterProps): JSX.Element => {
+const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 
   return (
-    <div {...props}>
-      Footer
-    </div>
+    <footer className={clsx(className, styles.footer)} {...props}>
+      <div>
+        Copyright © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+      </div>
+      <a href="#" target="_blank">Пользовательское соглашение</a>
+      <a href="#" target="_blank">Политика конфиденциальности</a>
+    </footer>
   );
 };
 
