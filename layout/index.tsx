0028e683aed7ca61a1d41ddbb63ad4f0f3f7799a
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-09-02 21:43:25 +0300

    Add app context

--- layout/index.tsx
+++ layout/index.tsx
@@ -7,6 +7,7 @@ import Footer from './Footer';
 import { LayoutProps } from './Layout.props';
 
 import styles from './Layout.module.css';
+import { AppContextProvider, IAppContext } from '../context/app.context';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 
@@ -22,12 +23,14 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
   );
 };
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FC<T>) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FC<T>) => {
   return function withLayoutComponent(props: T): JSX.Element {
     return (
-      <Layout>
-        <Component {...props} />
-      </Layout>
+      <AppContextProvider menu={props.menu} firstCategory={props.firstCategory} >
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      </AppContextProvider>
     );
   };
 };
