631d3bbbc00cc5f605c217d910b6cecab81c7a90
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-09-04 13:34:41 +0300

    Add basic search page

---
+++ pages/search.tsx
@@ -0,0 +1,36 @@
+import { GetStaticProps } from 'next';
+import React from 'react';
+import axios from 'axios';
+
+import { MenuItem } from '../interfaces/menu.interface';
+import { API } from '../helpers/api';
+import { withLayout } from '../layout';
+
+function Search(): JSX.Element {
+
+  return (
+    <>
+      Search
+    </>
+  );
+}
+
+export default withLayout(Search);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+    firstCategory
+  });
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
