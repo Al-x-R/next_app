e5c32c46c8a3fc7624e5c8e5370c051671740db3
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-09-04 13:52:38 +0300

    Add sidebar layout

--- layout/Sidebar/index.tsx
+++ layout/Sidebar/index.tsx
@@ -4,15 +4,18 @@ import clsx from 'clsx';
 import Menu from '../Menu';
 import { AppContext } from '../../context/app.context';
 import { SidebarProps } from './Sidebar.props';
+import Logo from '../logo.svg';
 
 import styles from './Sidebar.module.css';
 
 
-const Sidebar = ({...props}: SidebarProps): JSX.Element => {
+const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   const { menu, setMenu, firstCategory} = useContext(AppContext);
 
   return (
-    <div {...props}>
+    <div className={clsx(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>search</div>
       <Menu />
     </div>
   );
