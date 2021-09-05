c37be29c080dffca140cca195dc6a4f7adc50081
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-09-05 21:07:24 +0300

    Add Card component

---
+++ components/Card/index.tsx
@@ -0,0 +1,19 @@
+import React, { ForwardedRef, forwardRef } from 'react';
+import { CardProps } from './Card.props';
+import styles from './Card.module.css';
+import clsx from 'clsx';
+
+const Card = forwardRef(({ color = 'white', children, className, ...props }: CardProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+  return (
+    <div className={clsx(styles.card, className, {
+      [styles.blue]: color == 'blue'
+    })}
+         ref={ref}
+         {...props}
+    >
+      {children}
+    </div>
+  );
+});
+
+export default Card;
