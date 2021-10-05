55446c3d6f73b2e933363c09a811af0d791a3727
alexander <alexander.roshchin85@gmail.com>
2021-10-05 11:48:04 +0300

    Added Header layout with mobile version

--- layout/Header/index.tsx
+++ layout/Header/index.tsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
+import { useRouter } from 'next/router';
+import { motion, useReducedMotion } from 'framer-motion';
 
 import { HeaderProps } from './Header.props';
+import ButtonIcon from '../../components/ButtonIcon';
+import Sidebar from '../Sidebar';
+import Logo from '../logo.svg';
 
 import styles from './Header.module.css';
 
-const Header = ({ ...props }: HeaderProps): JSX.Element => {
+const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
+    const [isOpened, setIsOpened] = useState<boolean>(false);
+    const router = useRouter();
+    const shouldReduceMotion = useReducedMotion();
+
+    useEffect(() => {
+        setIsOpened(false);
+    }, [router]);
+
+    const variants = {
+        opened: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                stiffness: 20
+            }
+        },
+        closed: {
+            opacity: shouldReduceMotion ? 1 : 0,
+            x: '100%',
+        }
+    };
 
   return (
-    <div {...props}>
-      Header
-    </div>
+      <header className={clsx(className, styles.header)} {...props}>
+          <Logo />
+          <ButtonIcon appearance='white' icon='menu' onClick={() => setIsOpened(true)} />
+          <motion.div
+              className={styles.mobileMenu}
+              variants={variants}
+              initial={'closed'}
+              animate={isOpened ? 'opened' : 'closed'}
+          >
+              <Sidebar />
+              <ButtonIcon className={styles.menuClose} appearance='white' icon='close' onClick={() => setIsOpened(false)} />
+          </motion.div>
+      </header>
   );
 };
 
