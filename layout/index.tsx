f329be903afd1538024e96dd03b3286214f6efad
alexander <alexander.roshchin85@gmail.com>
2021-10-06 11:59:53 +0300

    Added the ability to skip the menu and go directly to the content

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,37 +1,57 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useState, KeyboardEvent } from 'react';
+import clsx from 'clsx';
 
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 import Up from '../components/Up';
-import { LayoutProps } from './Layout.props';
 
-import styles from './Layout.module.css';
+import { LayoutProps } from './Layout.props';
 import { AppContextProvider, IAppContext } from '../context/app.context';
 
-const Layout = ({ children }: LayoutProps): JSX.Element => {
+import styles from './Layout.module.css';
 
-  return (
-    <div className={styles.wrapper}>
-      <Header className={styles.header} />
-      <Sidebar className={styles.sidebar} />
-      <div className={styles.body}>
-        {children}
-      </div>
-      <Footer className={styles.footer} />
-        <Up />
-    </div>
-  );
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+    const [isSkipLinkDisplayed, setIsSkipLinkDisplayed] = useState<boolean>(false);
+    const bodyRef = useRef<HTMLDivElement>(null);
+
+    const skipMenuAction = (key: KeyboardEvent) => {
+        if (key.code == 'Space' || key.code == 'Enter') {
+            key.preventDefault();
+            bodyRef.current?.focus();
+        }
+        setIsSkipLinkDisplayed(false);
+    };
+
+
+    return (
+        <div className={styles.wrapper}>
+            <a onFocus={() => setIsSkipLinkDisplayed(true)}
+               tabIndex={0}
+               className={clsx(styles.skipLink, {
+                   [styles.displayed]: isSkipLinkDisplayed
+               })}
+               onKeyDown={skipMenuAction}
+            >Сразу к содержанию</a>
+            <Header className={styles.header} />
+            <Sidebar className={styles.sidebar} />
+            <div className={styles.body} ref={bodyRef} tabIndex={0}>
+                {children}
+            </div>
+            <Footer className={styles.footer} />
+            <Up />
+        </div>
+      );
 };
 
 export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FC<T>) => {
-  return function withLayoutComponent(props: T): JSX.Element {
-    return (
-      <AppContextProvider menu={props.menu} firstCategory={props.firstCategory} >
-        <Layout>
-          <Component {...props} />
-        </Layout>
-      </AppContextProvider>
-    );
-  };
+    return function withLayoutComponent(props: T): JSX.Element {
+        return (
+          <AppContextProvider menu={props.menu} firstCategory={props.firstCategory} >
+            <Layout>
+              <Component {...props} />
+            </Layout>
+          </AppContextProvider>
+        );
+    };
 };
