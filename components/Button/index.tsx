6256e548a4e69062f8c3bca3db7b9a99439caadc
alexander <alexander.roshchin85@gmail.com>
2021-10-06 10:12:27 +0300

    Added animation for button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
+import { motion } from 'framer-motion';
 
 import { ButtonProps } from './Button.props';
 import ArrowIcon from './arrow.svg';
@@ -8,23 +9,24 @@ import styles from './Button.module.css';
 
 const Button = ({ children, appearance, className, arrow = 'none', ...props }: ButtonProps): JSX.Element => {
   return (
-    <button
-      className={clsx(styles.button, className, {
-        [styles.primary]: appearance === 'primary',
-        [styles.ghost]: appearance === 'ghost',
-      })}
-      {...props}
+    <motion.button
+        whileHover={{ scale: 1.05 }}
+        className={clsx(styles.button, className, {
+            [styles.primary]: appearance === 'primary',
+            [styles.ghost]: appearance === 'ghost',
+        })}
+        {...props}
     >
-      {children}
-      {arrow !== 'none' && <span
-        className={clsx(styles.arrow, {
-        [styles.down]: arrow === 'down',
-        [styles.right]: arrow === 'right'
-      })}>
-        <ArrowIcon />
-      </span>
-      }
-    </button>
+        {children}
+        {arrow !== 'none' && <span
+            className={clsx(styles.arrow, {
+            [styles.down]: arrow === 'down',
+            [styles.right]: arrow === 'right'})}
+        >
+            <ArrowIcon />
+        </span>
+        }
+    </motion.button>
   );
 };
 
